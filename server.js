36867311e3cf5b607e9e7273138ed9d014637290
Daniel Merkl <dan.merkl.90@gmail.com>
2019-10-08 21:26:06 -0500

    got a few things done

--- server.js
+++ server.js
@@ -49,42 +49,44 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 
 // Routes
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.nytimes.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+// // A GET route for scraping the echoJS website
+// app.get("/scrape", function(req, res) {
+//   // First, we grab the body of the html with axios
+//   axios.get("http://www.nytimes.com/").then(function(response) {
+//     // Then, we load that into cheerio and save it to $ for a shorthand selector
+//     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $(".css-6p6lnl").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+//     // Now, we grab every h2 within an article tag, and do the following:
+//     $(".css-6p6lnl").each(function(i, element) {
+//       // Save an empty result object
+//       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .find("h2")
-        .text()
-        .trim();
+//       // Add the text and href of every link, and save them as properties of the result object
+//       result.title = $(this)
+//         .find("h2")
+//         .text()
+//         .trim();
 
-      result.summary = $(this)
-        .find("p")
-        .text()
-        .trim();
+//       result.summary = $(this)
+//         .find("p")
+//         .text()
+//         .trim();
 
-        result.link = "https://www.nytimes.com" + $(this)
-        .find("a")
-        .attr("href")
+//         result.link = "https://www.nytimes.com" + $(this)
+//         .find("a")
+//         .attr("href")
 
-      // Create a new Article using the `result` object built from scraping
-      const entry = new Article(result)
-      entry.save(function(err, doc){
-          if (err){
-              console.log(err)
-          }
-        })
-      })
-      console.log("Scrape Complete")
+//       // Create a new Article using the `result` object built from scraping
+//       const entry = new Article(result)
+//       entry.save(function(err, doc){
+//           if (err){
+//               console.log(err)
+//           }
+//         })
+//       })
+//       console.log("Scrape Complete")
+//     })
+// })
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
